72fb15627e54a7f2e497664394a601328eddc27d
danh <nguyenhoaidanh2096@gmail.com>
2019-07-05 00:06:11 +0700

    deploy

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/sendFiles', function (req, res) {
     var files = isArray(req.files.files) ? req.files.files : [req.files.files]
     var promises = []
     files.forEach(f => {
-        var fName = '/upload/' + String(1000000000 * Math.random()) + f.name
+        var fName = '/upload/' + f.name
         var pathName = __dirname + fName
         var pm = new Promise((resolve, reject) => {
             f.mv(pathName, function (err) {
