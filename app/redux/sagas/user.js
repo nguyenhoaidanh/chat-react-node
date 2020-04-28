ef3a695f09f1d920107c328667e9544ecadc3291
nguyenhoaidanh <nguyenhoaidanh2096@gmail.com>
2020-04-28 21:57:50 +0700

    rebuild

--- app/redux/sagas/user.js
+++ app/redux/sagas/user.js
@@ -4,5 +4,6 @@ import * as type from '../actions/action-types';
  * Listener lắng nghe GET_LIST_USER_ONLINE để xử lý
  */
 export default function* user() {
+  //
   yield takeLatest(type.APP.GET_LIST_USER_ONLINE, requestListFriendAsync);
 }
