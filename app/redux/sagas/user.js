2ed18f199c35f78ffdff09f763157174d7de6e3d
nguyenhoaidanh <nguyenhoaidanh2096@gmail.com>
2020-04-28 22:18:37 +0700

    rebuild

--- app/redux/sagas/user.js
+++ app/redux/sagas/user.js
@@ -7,3 +7,20 @@ export default function* user() {
   //
   yield takeLatest(type.APP.GET_LIST_USER_ONLINE, requestListFriendAsync);
 }
+function* requestListFriendAsync() {
+  yield put({type: type.APP.GET_LIST_ITEM_START});
+  const user = yield call(getListFriend);
+  yield put({type: type.APP.GET_LIST_ITEM_END, payload: user});
+}
+function getListFriend() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          email: 'somemockemail@email.com',
+          repository: 'http://github.com/username'
+        }
+      ]);
+    }, 1000);
+  });
+}
